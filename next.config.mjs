cb9e39bcd34999a3cfa1050077f81467bbb1888a
David Morales <david.morales@ppm.com.ec>
2024-06-28 16:24:27 -0600

    add anticlickhacking headers

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,23 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    poweredByHeader: false
+    poweredByHeader: false,
+    async headers() {
+        return [
+            {
+                source: '/(.*)',
+                headers: [
+                    {
+                        key: 'X-Frame-Options',
+                        value: 'DENY'
+                    },
+                    {
+                        key: 'Content-Security-Policy',
+                        value: "frame-ancestors 'none';"
+                    }
+                ]
+            }
+        ]
+    }
 };
 
 export default nextConfig;
